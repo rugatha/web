cf2e66514af404a376357a94befe86343a1a87e0
Rugatha D&D <chern.albert@gmail.com>
2026-01-18 13:22:26 +0800

    Update

---
+++ shared/firebase.config.js
@@ -0,0 +1,9 @@
+window.RUGATHA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBvVXMxGHHJH2KCGhi5AjJeu-7_48irc1U",
+  authDomain: "rugatha-87e15.firebaseapp.com",
+  projectId: "rugatha-87e15",
+  storageBucket: "rugatha-87e15.firebasestorage.app",
+  messagingSenderId: "1022766499204",
+  appId: "1:1022766499204:web:a4a4c318e16ce37809c9d1",
+  databaseURL: "https://rugatha-87e15-default-rtdb.firebaseio.com/"
+};
