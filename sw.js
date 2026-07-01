4883f5f6c5e9bb8aa7e46a03b9a7c8eca996460d
Rugatha D&D <chern.albert@gmail.com>
2026-07-01 08:37:35 +0800

    Fix PWA icon to full size

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "rugatha-pwa-v8";
+const CACHE_VERSION = "rugatha-pwa-v9";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
