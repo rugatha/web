d4aaea17e6d003288d51e2890fcf09e8886f9006
Rugatha D&D <chern.albert@gmail.com>
2026-07-01 12:20:44 +0800

    Change icon background to #e3d0ab

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "rugatha-pwa-v9";
+const CACHE_VERSION = "rugatha-pwa-v11";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
